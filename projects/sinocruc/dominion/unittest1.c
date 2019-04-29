ceab3a9c1f986e19111faf37e20a5dcdacfdfc44
callmecarlito <callmecarlito@yahoo.com>
2019-04-29 15:19:27 -0700

    Update unittest1.c

--- projects/sinocruc/dominion/unittest1.c
+++ projects/sinocruc/dominion/unittest1.c
@@ -16,19 +16,36 @@
 #include <assert.h>
 #include "rngs.h"
 #include "dominion_refactored.h"
+#include "interface.h"
+
+#define assertrue(bool) if(bool) printf("TEST SUCCESSFULLY COMPLETED.\n"); else printf("TEST FAILED: '");
+
+int main(){
+
+	int seed = 1000;
+    int numPlayer = 2;
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+               , remodel, smithy, village, baron, great_hall};
+    struct gameState G;
+    int r;
+
+    memset(&G, 23, sizeof(struct gameState));   // clear the game state
+    r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
+    asserttrue(r==0);
+
+    printGameState(G);
+
+	printf("unittest1 result\n")
+
+	return 0;
+}
 
 // set NOISY_TEST to 0 to remove printfs from output
 //#define NOISY_TEST 1
 
 //#define assertrue(bool)
 
-int main(){
 
 //#if (NOISY_TEST == 1)
     //printf("G.coins = %d, expected = %d\n", G.coins, handCount * 1 + bonus);
 //#endif 
-
-	printf("unittest1 result\n")
-
-	return 0;
-}
