9b5a197ecfc66bca534ae687ff05f18f79eac82f
callmecarlito <callmecarlito@yahoo.com>
2019-04-30 16:16:42 -0700

    Update unittest1.c

--- projects/sinocruc/dominion/unittest1.c
+++ projects/sinocruc/dominion/unittest1.c
@@ -1,11 +1,13 @@
 /* -----------------------------------------------------------------------
- * *******This is a unit test for function drawCard() found in the file
- * *******dominion.c
+ * This is a unit test for function call of smithy_call() within cardEffect()
+ * of th dominion.c soure file. When thi function is called, subroutine 
+ * drawCard() should be called three times, followed by discardCard(), and if
+ * no error occurs, a value of 0 should be returned. This unit test checks 
  *
- * *******Include the following lines in your makefile:
+ * Include the following lines in your makefile:
  *
- * *******testUpdateCoins: testUpdateCoins.c dominion.o rngs.o
- * *******     gcc -o testUpdateCoins -g  testUpdateCoins.c dominion.o rngs.o $(CFLAGS)
+ * unittest1: unittest1.c dominion.o rngs.o
+ *      gcc -o unittest1 -g  unittest1.c dominion.o rngs.o $(CFLAGS)
  * -----------------------------------------------------------------------
  */
 
@@ -18,39 +20,42 @@
 #include "dominion_refactored.h"
 #include "interface.h"
 
+// set NOISY_TEST to 0 to remove printfs from output
+#define NOISY_TEST 1
+
+#define assertrue(bool) if(bool) printf("TEST SUCCESSFULLY COMPLETED.\n"); else printf("TEST FAILED:' " #bool " ' on line %d.\n", __LINE__);
+
 int main(){
 
 	int seed = 1000;
     int numPlayer = 2;
     int k[10] = {adventurer, council_room, feast, gardens, mine
                , remodel, smithy, village, baron, great_hall};
-    struct gameState G, testG;
+    struct gameState G, testG; 
     int r, s;
-    int drawnCards = 4, discardedCards = 1;
+    int drawnCards = 3, discardedCards = 1;
    	int handPos = 0, currentPlayer = 0;
 
     memset(&G, 23, sizeof(struct gameState));   // clear the game state
     r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
     assert(r==0);
-    memcpy(&testG, &G, sizeof(struct gameState));
-    //printf("BEFORE: G.handCount: %d ,testG.handCount: %d \n", G.handCount[currentPlayer], testG.handCount[currentPlayer]);
+    memcpy(&testG, &G, sizeof(struct gameState)); //make a copy of the current game state
 
+#if (NOISY_TEST == 1)
+    printf("Before function call:\n G.handCount: %d ,testG.handCount: %d \n", G.handCount[currentPlayer], testG.handCount[currentPlayer]);
+#endif
+    
     s = smithy_call(handPos, currentPlayer, &G);
     assert(s==0);
-    assert(G.handCount[currentPlayer] == (testG.handCount[currentPlayer] + drawnCards - discardedCards));
-    //printf("AFTER: G.handCount: %d ,testG.handCount: %d \n", G.handCount[currentPlayer], testG.handCount[currentPlayer]);
-
-	printf("UNITTEST1 PASSED\n");
+    
+#if (NOISY_TEST == 1)
+    printf("After function call:\n G.handCount: %d ,testG.handCount: %d \n", G.handCount[currentPlayer], testG.handCount[currentPlayer]);
+#endif
+  
+  	asserttrue(G.handCount[currentPlayer] == (testG.handCount[currentPlayer] + drawnCards - discardedCards));
+  	
+	printf("Unit Test 1 complete...\n");
 
 	return 0;
 }
-
-// set NOISY_TEST to 0 to remove printfs from output
-//#define NOISY_TEST 1
-
 //#define assertrue(bool) if(bool) printf("TEST SUCCESSFULLY COMPLETED.\n"); else printf("TEST FAILED: '");
-
-
-//#if (NOISY_TEST == 1)
-    //printf("G.coins = %d, expected = %d\n", G.coins, handCount * 1 + bonus);
-//#endif 
